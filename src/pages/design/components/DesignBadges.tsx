6ae7dfaacf1b592f8b443ee03f3fe33005d874e4
vinsmake <ps.enrique.e@gmail.com>
2024-11-09 08:36:31 -0600

    Project updated

--- src/pages/design/components/DesignBadges.tsx
+++ src/pages/design/components/DesignBadges.tsx
@@ -7,6 +7,9 @@ export const DesignBadges = () => {
             <Badge variant="default">Badge</Badge>
             <Badge variant="secondary">Badge</Badge>
             <Badge variant="outline">Badge</Badge>
+
+
+            <div className="inline-flex items-center rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 border-transparent bg-primary text-primary-foreground hover:bg-primary/80">Fake</div>
         </>
     )
 }
