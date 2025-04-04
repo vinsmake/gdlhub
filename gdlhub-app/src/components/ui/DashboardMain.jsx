2ed667432d2fbded3212018c9ccacdac446fe4e2
vinsmake <ps.enrique.e@gmail.com>
2025-04-04 14:15:12 -0600

    Project updated to Github Pages

--- gdlhub-app/src/components/ui/DashboardMain.jsx
+++ gdlhub-app/src/components/ui/DashboardMain.jsx
@@ -1,9 +1,14 @@
 export const DashboardMain = ({ children }) => {
     return (
-        <>
-            <div className="w-4/5 color--bg-neutral-800 rounded-3xl p-4">
-
-            </div>
-        </>
-    )
-}
+      <div className="w-4/5 bg-neutral-800 rounded-3xl p-6 text-white space-y-6">
+        {/* Saludo al usuario */}
+        <h1 className="text-2xl font-semibold">
+          ¡Hola, Enrique Plascencia!
+        </h1>
+  
+        {/* Contenido principal */}
+        <div>{children}</div>
+      </div>
+    );
+  };
+  
