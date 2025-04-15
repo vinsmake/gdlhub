cd58419b59e0d97939595b84ab6fd8bb6b62339d
vinsmake <ps.enrique.e@gmail.com>
2025-04-15 14:00:26 -0600

    structure added

--- gdlhub-app/src/components/ui/DashboardMain.jsx
+++ gdlhub-app/src/components/ui/DashboardMain.jsx
@@ -1,14 +1,7 @@
 export const DashboardMain = ({ children }) => {
-    return (
-      <div className="w-4/5 bg-neutral-800 rounded-3xl p-6 text-white space-y-6">
-        {/* Saludo al usuario */}
-        <h1 className="text-2xl font-semibold">
-          ¡Hola, Enrique Plascencia!
-        </h1>
-  
-        {/* Contenido principal */}
-        <div>{children}</div>
-      </div>
-    );
-  };
-  
+  return (
+    <div className="w-4/5 bg-neutral-800 rounded-3xl p-6 text-white space-y-6">
+      {children}
+    </div>
+  );
+};
