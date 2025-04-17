e28f592827d9e2fcbb1c2bd70f8ab225c8326e4f
vinsmake <ps.enrique.e@gmail.com>
2025-04-17 12:58:28 -0600

    YA FUNCIONA GHPAGES

--- gdlhub-app/src/components/ui/DashboardMain.jsx
+++ gdlhub-app/src/components/ui/DashboardMain.jsx
@@ -2,7 +2,7 @@ export const DashboardMain = ({ children }) => {
   return (
     <div className="w-4/5 bg-neutral-800 rounded-3xl p-6 text-white space-y-6">
       {children}
-      Another fucking test omg why is not working
+      hasta este punto, esta funcionando
     </div>
   );
 };
