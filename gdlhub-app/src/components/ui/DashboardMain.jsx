fad6150f03b095f9bbbaaff73d5973b6fae978ba
vinsmake <ps.enrique.e@gmail.com>
2025-05-06 10:25:22 -0600

    added /restaurants request to front

--- gdlhub-app/src/components/ui/DashboardMain.jsx
+++ gdlhub-app/src/components/ui/DashboardMain.jsx
@@ -1,8 +1,11 @@
+import { RestaurantsList } from "../pages/RestaurantsPage";
+
 export const DashboardMain = ({ children }) => {
   return (
     <div className="w-4/5 bg-neutral-800 rounded-3xl p-6 text-white space-y-6">
       {children}
       hasta este punto, esta funcionando
+      <RestaurantsList />
     </div>
   );
 };
