fc9130ebc6bf90a9910022b8c7886f53c9d8c7a7
vinsmake <ps.enrique.e@gmail.com>
2025-05-06 10:34:39 -0600

    CORS fixed

--- gdlhub-api/src/index.js
+++ gdlhub-api/src/index.js
@@ -3,9 +3,12 @@ import userRoutes from "./routes/users.routes.js";
 import restaurantRoutes from "./routes/restaurants.routes.js";
 import { PORT } from "./config.js";
 import morgan from "morgan";
+import cors from "cors";
+
 
 const app = express();
 
+app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(userRoutes);
