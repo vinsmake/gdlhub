8daf1a458d08936627bbff131a971cc31da7c687
vinsmake <ps.enrique.e@gmail.com>
2025-05-05 21:28:32 -0600

    damn everything is working i love here

--- gdlhub-api/src/index.js
+++ gdlhub-api/src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import userRoutes from "./routes/users.routes.js";
+import restaurantRoutes from "./routes/restaurants.routes.js";
 import { PORT } from "./config.js";
 import morgan from "morgan";
 
@@ -8,6 +9,7 @@ const app = express();
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(userRoutes);
+app.use(restaurantRoutes);
 
 app.listen(PORT, () => {
   console.log(`🚀 Server is running on http://localhost:${PORT}`);
