5a3d3ee7170bb39a74554d4f61dccabea48fa5ed
vinsmake <ps.enrique.e@gmail.com>
2025-05-25 08:52:46 -0600

    min h added

--- gdlhub-app/src/components/ui/DashboardMain.jsx
+++ gdlhub-app/src/components/ui/DashboardMain.jsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 export const DashboardMain = ({ children }) => {
   return (
-    <div className="w-4/5 bg-neutral-800 rounded-3xl p-6 text-white space-y-6">
+<div className="w-4/5 min-h-screen bg-neutral-800 rounded-3xl p-6 text-white space-y-6">
       {children}
       <Outlet />
     </div>
