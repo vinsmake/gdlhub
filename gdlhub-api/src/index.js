620ee76169be3715e5144941df12e2d50388fa77
vinsmake <ps.enrique.e@gmail.com>
2025-05-25 08:35:10 -0600

    connection to /img/user added

--- gdlhub-api/src/index.js
+++ gdlhub-api/src/index.js
@@ -4,7 +4,12 @@ import restaurantRoutes from "./routes/restaurants.routes.js";
 import { PORT } from "./config.js";
 import morgan from "morgan";
 import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
 
+// Configurar ruta estática de profile pictures
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
@@ -13,6 +18,7 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(userRoutes);
 app.use(restaurantRoutes);
+app.use("/img/user", express.static(path.resolve(process.cwd(), "db/img/user")));
 
 app.listen(PORT, () => {
   console.log(`🚀 Server is running on http://localhost:${PORT}`);
