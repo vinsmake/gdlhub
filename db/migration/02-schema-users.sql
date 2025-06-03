c97a25206bf567a1ef3d587bcd1f369be6a1b447
vinsmake <ps.enrique.e@gmail.com>
2025-06-03 09:54:34 -0600

    feed shows commets

--- db/migration/02-schema-users.sql
+++ db/migration/02-schema-users.sql
@@ -7,19 +7,6 @@ CREATE TABLE IF NOT EXISTS users (
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE IF NOT EXISTS user_follows (
-  follower_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-  followed_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-  PRIMARY KEY (follower_id, followed_id),
-  CHECK (follower_id <> followed_id)
-);
-
-CREATE TABLE IF NOT EXISTS favorite_restaurants (
-  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-  restaurant_id INTEGER REFERENCES restaurants(id) ON DELETE CASCADE,
-  PRIMARY KEY (user_id, restaurant_id)
-);
-
 CREATE TABLE IF NOT EXISTS favorite_menu_items (
   user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   menu_item_id INTEGER REFERENCES menu_items(id) ON DELETE CASCADE,
--- db/migration/03-schema-social.sql
+++ db/migration/03-schema-social.sql
@@ -1,3 +1,20 @@
+CREATE TABLE IF NOT EXISTS favorite_restaurants (
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  restaurant_id INTEGER REFERENCES restaurants(id) ON DELETE CASCADE,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (user_id, restaurant_id)
+);
+
+
+CREATE TABLE IF NOT EXISTS user_follows (
+  follower_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  followed_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (follower_id, followed_id),
+  CHECK (follower_id <> followed_id)
+);
+
+
 CREATE TABLE IF NOT EXISTS comments (
   id SERIAL PRIMARY KEY,
   user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
